5366656d65883276f1c9c6701585108f5a8dbabf
Helene <helene@outlook.com>
2015-11-24 18:47:31 -0700

    01

--- 01_equality/src.js
+++ 01_equality/src.js
@@ -1,5 +1,39 @@
 module.exports = {
   sameLength: function(string1, string2) {
-    // your code here:
-  }
+    if (string1.length == string2.length){
+    return true;}
+    else {
+      return false;
+    }
+  },
+
+  firstGreater: function(a, b){
+    if (a > b){return true;}
+    else {
+      return false;
+    }
+
+  },
+
+  firstGreaterOrEqualTo: function(a, b){
+  if (a >= b){return true;}
+  else {
+    return false;
+      }
+  },
+  firstLengthGreater: function(a, b){
+    if (a.length > b.length){return true;}
+    else {
+      return false;
+        }
+
+
+  },
+secondGreater: function(a, b){
+  if (b > a){return true;}
+  else {
+    return false;
+      }
+
+}
 }
