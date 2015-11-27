d53f7baa700937ab13430db8fd66ea3112ca5b7f
Helene <helene@outlook.com>
2015-11-26 20:20:57 -0700

    04 done

--- 04_objects/src.js
+++ 04_objects/src.js
@@ -1,5 +1,56 @@
 module.exports = {
   person: function() {
-    // your code here
+  var person = {firstName: "John", lastName: "Mayer"};
+
+return person;
+
+  },
+
+  sweetnessProperty: function(obj){
+  return obj["sweetness"];},
+
+
+
+
+savorynessProperty: function(obj){
+return obj["savory-ness"];
+
+},
+
+keys: function(obj){
+return Object.keys(obj);
+
+
+},
+
+sortedKeys: function(obj){
+// var keys =[]
+// if (Object.keys({}).length ==0){
+//   return keys
+// };
+// else{
+//   keys = Object.keys(obj);
+//     return keys.sort;}
+
+var array = [];
+ for (var key in obj) {
+   array.push(key);
+ }
+ array.sort();
+ return array;
+},
+
+
+
+
+
+
+
+reverseSortedKeys: function(obj){
+    var keys =[];
+    keys = Object.keys(obj);
+    keys.sort();
+    keys.reverse();
+    return keys;
   }
-}
+  }
