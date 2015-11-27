5e2b47443352d0d181c1368237cd7c8e59500804
Helene <helene@outlook.com>
2015-11-26 20:32:13 -0700

    05 done

--- 05_fix_broken_code/src.js
+++ 05_fix_broken_code/src.js
@@ -6,13 +6,14 @@
 
 module.exports = {
   addition: function(a,b) {
-    a + b;
+    return a + b;
   },
 
   matches: function(first, second) {
+
     var result;
 
-    if (first = second) {
+    if (first === second) {
       result = true;
     } else {
       result = false;
@@ -22,8 +23,8 @@ module.exports = {
   },
 
   fizzBuzz: function(number) {
-    var divisibleByThree = number % 3,
-        divisibleByFive = number % 5,
+    var divisibleByThree = number % 3 == 0,
+        divisibleByFive = number % 5 == 0,
         output = number;
 
     if (divisibleByThree && divisibleByFive) {
@@ -38,17 +39,21 @@ module.exports = {
   },
 
   switcher: function(word) {
-    var output;
+    var output ="";
 
     switch (word) {
       case 'foo':
         output = 'bar';
+        break;
       case 'nope':
         output = 'yup';
+        break;
       default:
         output = 'never output anything. ever!'
+        break;
     };
 
     return output;
+
   }
 }
